d02dd591491aa9079702d0498ee772c48328313f
Tung <21125145@student.hcmus.edu.vn>
2022-07-20 18:24:35 +0700

    Them form edit

--- DictionaryGUI/edit.h
+++ DictionaryGUI/edit.h
@@ -34,6 +34,12 @@ namespace DictionaryGUI {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Button^ button1;
 
 	private:
 		/// <summary>
@@ -48,11 +54,76 @@ namespace DictionaryGUI {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"edit";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(13, 13);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(70, 13);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Current word:";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(89, 13);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(28, 13);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"abc";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(12, 54);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(77, 13);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"New definition:";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(15, 80);
+			this->textBox1->Multiline = true;
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(336, 134);
+			this->textBox1->TabIndex = 3;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(264, 221);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(87, 28);
+			this->button1->TabIndex = 4;
+			this->button1->Text = L"Confirm";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// edit
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(363, 261);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->MaximizeBox = false;
+			this->MinimizeBox = false;
+			this->Name = L"edit";
+			this->Text = L"edit";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
 	};
